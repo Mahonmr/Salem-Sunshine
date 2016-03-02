23cd80a4f148d6995506cad65379193ca8bd2e6a
Jeffrey Ruder and Mike Mahoney <ruderjt@gmail.com>
2016-03-01 17:21:27 -0800

    Add more form input validation

--- app/components/lookup-form/component.js
+++ app/components/lookup-form/component.js
@@ -7,11 +7,11 @@ export default Ember.Component.extend({
       var params = {
         street: this.get('street'),
         city: this.get('city'),
-        zip: this.get('zip')
+        zip: this.get('zip'),
       };
-      this.set('street', undefined)
-      this.set('city', undefined),
-      this.set('zip', undefined),
+      this.set('street', "")
+      this.set('city', ""),
+      this.set('zip', ""),
       this.sendAction('submitAddress', params);
     }
   }
--- app/lookup/route.js
+++ app/lookup/route.js
@@ -19,6 +19,7 @@ export default Ember.Route.extend({
     },
 
     submitAddress(params) {
+      console.log(params)
       var self = this;
 
       var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' +
